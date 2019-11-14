09430a18880830d76838af373168dffbb2654c57
Marta <marta.wiese5@gmail.com>
2019-11-14 12:39:17 +0100

    wersja z scss

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('sass', function() {
-    return gulp.src('scss/*.scss')
+    return gulp.src('scss/**/*.scss')
         .pipe(sourcemaps.init())
         .pipe(sass().on('error', sass.logError))
         .pipe(sourcemaps.write())
